740f07886ca1201e5f2e1e293f5aae5b1a2abd4a
Ayush <128382463+ayushrs@users.noreply.github.com>
2026-04-18 14:03:54 +0530

    Changes
    Add a comment section for API routes.

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
